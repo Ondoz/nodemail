fdc867df5740a43a46f3cabd3f50e11735402ed9
Lukman Firdaus <lukmanfrdas@gmail.com>
2021-02-11 09:27:17 +0700

    update

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,3 +1,5 @@
+const mustache = require('mustache')
+const fs = require('fs');
 
 exports.index = async (req, res)  => {
     
@@ -6,16 +8,19 @@ exports.index = async (req, res)  => {
     var to      = req.body.to;
     var subject = req.body.subject;
     var message = req.body.message;
+    var data    = req.body.data;
+    var template= req.body.template;
+
     const nodemailer = require("nodemailer");
 
-    try{
+    // try{
         let testAccount = await nodemailer.createTestAccount();
 
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
             host: "mail.oaktree.id",
-            port: 465,
-            secure: true, // true for 465, false for other ports
+            port: 587,
+            secure: false, // true for 465, false for other ports
             auth: {
                 user: "noreply@oaktree.id", // generated ethereal user
                 pass: "U_h1t9f5", // generated ethereal password
@@ -26,30 +31,41 @@ exports.index = async (req, res)  => {
             
         });
 
-        // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: from, // sender address
-            to: to, // list of receivers
-            subject: subject, // Subject line
-            text: message, // plain text body
-            html: message, // html body
+        const mailOptions = {
+            from: from,
+            to: to,
+            subject: subject,
+        };
+
+        if(template !== undefined){
+            var template = fs.readFileSync(template,{encoding:'utf-8'});
+            mailOptions.html = mustache.render(template, data)
+        } else {
+            mailOptions.text = message
+        }
+
+        await transporter.sendMail(mailOptions , (err, info) => {
+            if(err){
+                console.log(err);
+            }
+            res.json({
+                status: "success",
+                message: "Email successfully",
+                data: info
+            })
         });
 
-        console.log("Message sent: %s", info.messageId);
+        // console.log("Message sent: %s", info.messageId);
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
         // Preview only available when sending through an Ethereal account
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        res.json({
-            status: "success",
-            message: "Email successfully",
-            data: null
-        })  
-    } catch(err){
-        console.log(err)
-        res.status(500).send(err)
-    }
+
+    // } catch(err){
+    //     console.log(err)
+    //     res.status(500).send(err)
+    // }
     
      
 }

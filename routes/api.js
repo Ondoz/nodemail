fc86fd27d56d42b271bb7ad53b77616f1501195f
Lukman Firdaus <lukmanfrdas@gmail.com>
2021-03-02 18:46:06 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,15 @@
 let router = require('express').Router()
 
 const emailController = require('../controllers/emailController')
+const waController = require('../controllers/waController')
 
 
 router.route('/email')
     .post(emailController.index)
+
+
+router.route('/wa/create/:name')
+    .get(waController.index)
 
 
 // Export API routes
